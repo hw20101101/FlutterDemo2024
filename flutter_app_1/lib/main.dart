a3dbc7d169ec5145dcd73cee108ff3db7b451c2b
hwacdx <1296324895@qq.com>
2024-09-14 20:05:01 +0800

    导航到新页面并返回

--- flutter_app_1/lib/main.dart
+++ flutter_app_1/lib/main.dart
@@ -8,7 +8,11 @@ import 'package:provider/provider.dart';
 void main() {
   // 应用的起点
   //runApp(MyApp());
-  runApp(TabApp());
+  //runApp(TabApp());
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
 }
 
 // ================= Tabbar ↓ =================
@@ -260,6 +264,52 @@ class BigCard extends StatelessWidget {
           style: style,
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
+      ),
+    );
+  }
+}
+
+// ================= Navigator ↓ =================
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              // 跳转到第二个路由
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()),
+              );
+            },
+            child: const Text('Open route')),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              //返回到上一个路由
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!')),
       ),
     );
   }
