0df9c72b372e94b53f83371311ef2612af79ad67
hwacdx <1296324895@qq.com>
2024-09-15 23:11:24 +0800

    从新页面返回数据

--- flutter_app_1/lib/main.dart
+++ flutter_app_1/lib/main.dart
@@ -15,15 +15,20 @@ void main() {
   //   home: FirstRoute(),
   // ));
 
-  runApp(MaterialApp(
-    title: 'Passing Data',
-    home: TodosScreen(
-      todos: List.generate(
-        20,
-        (i) => Todo(
-            'Todo $i', 'A description of what needs to be done for Todo $i'),
-      ),
-    ),
+  // runApp(MaterialApp(
+  //   title: 'Passing Data',
+  //   home: TodosScreen(
+  //     todos: List.generate(
+  //       20,
+  //       (i) => Todo(
+  //           'Todo $i', 'A description of what needs to be done for Todo $i'),
+  //     ),
+  //   ),
+  // ));
+
+  runApp(const MaterialApp(
+    title: 'Returning Data',
+    home: HomeScreen(),
   ));
 }
 
@@ -388,6 +393,96 @@ class DetailScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         //内容
         child: Text('DetailScreen:\n\n${todo.description}'),
+      ),
+    );
+  }
+}
+
+// ================= 主屏界面 ↓ =================
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Returning Data Demo'),
+      ),
+      body: Center(
+        child: SelectionButton(),
+      ),
+    );
+  }
+}
+
+class SelectionButton extends StatefulWidget {
+  const SelectionButton({super.key});
+
+  @override
+  State<SelectionButton> createState() => _SelectionButton();
+}
+
+class _SelectionButton extends State<SelectionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        //当点击按钮时 跳转到选择界面
+        onPressed: () {
+          _navigateAndDisplaySelection(context);
+        },
+        child: const Text('Pick an option, any option!'));
+  }
+
+  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SelectionScreen()),
+    );
+
+    if (!context.mounted) return;
+
+    //用一个 snackbar 显示结果
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text('选中的结果：$result')));
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  //..
+                  Navigator.pop(context, 'Yep!');
+                },
+                child: const Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  //
+                  Navigator.pop(context, 'Nope.');
+                },
+                child: const Text('Nope.'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
