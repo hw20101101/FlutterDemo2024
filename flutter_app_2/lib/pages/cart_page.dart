ede93ece85589e55666ab7bf9e437aff639c96f7
hwacdx <1296324895@qq.com>
2024-11-04 23:53:26 +0800

    视频的播放和暂停

--- flutter_app_2/lib/pages/cart_page.dart
+++ flutter_app_2/lib/pages/cart_page.dart
@@ -1,9 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-// 购物车界面
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<CartPage> {
+  late VideoPlayerController _controller;
+  late Future<void> _initVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4'));
+
+    _initVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('购物车-页面')));
+    //return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Butterfly Video'),
+      ),
+      body: FutureBuilder(
+        future: _initVideoPlayerFuture,
+        builder: (context, snapshot) {
+          //---
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ), //播放按钮
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child:
+            Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
+      ),
+    );
   }
 }
