bef04726ce55697e7b83d907338a49d62082f0b5
hwacdx <1296324895@qq.com>
2024-11-05 23:34:41 +0800

    短时状态 (ephemeral)

--- flutter_app_2/lib/pages/member_page.dart
+++ flutter_app_2/lib/pages/member_page.dart
@@ -1,9 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // 会员中心界面
-class MemberPage extends StatelessWidget {
+
+class MemberPage extends StatefulWidget {
+  const MemberPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MemberPageState();
+}
+
+class _MemberPageState extends State<MemberPage> {
+  //短暂状态( app 中的其他部分不需要访问 _index，这个变量只会在 MemberPage widget 中改变)
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('会员中心')));
+    return Scaffold(
+        //顶部导航栏
+        appBar: AppBar(
+          title: const Text('我的'),
+        ),
+        body: Center(
+          child: CupertinoButton(
+              child: Text('你敢点我试试? : $_index'),
+              //按钮点击事件
+              onPressed: () {
+                setState(() {
+                  _index += 1;
+                });
+              }),
+        ));
   }
 }
