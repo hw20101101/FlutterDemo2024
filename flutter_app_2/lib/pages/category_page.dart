449957526ee686c4b7cb7071baefc79d585dfcd8
hwacdx <1296324895@qq.com>
2024-11-10 08:15:34 +0800

    测试异步函数(错误示例)

--- flutter_app_2/lib/pages/category_page.dart
+++ flutter_app_2/lib/pages/category_page.dart
@@ -1,9 +1,26 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 // 分类界面
 class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //测试异步函数
+    testAsync();
+
     return Scaffold(body: Center(child: Text('分类页面')));
   }
 }
+
+void testAsync() {
+  print(createOrderMessage());
+}
+
+String createOrderMessage() {
+  var order = fetchUserOrder();
+  return "your order is: $order";
+}
+
+Future<String> fetchUserOrder() =>
+    Future.delayed(const Duration(seconds: 2), () => 'lagre latte');
