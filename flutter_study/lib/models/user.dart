cc14206bd82c6bbbd3b60aa87111edd2cfcd48d0
hwacdx <1296324895@qq.com>
2024-12-05 23:43:20 +0800

    JSON 和序列化数据

--- flutter_study/lib/models/user.dart
+++ flutter_study/lib/models/user.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+part 'user.g.dart';
+
 class User {
   String? name;
   int? age;
@@ -8,4 +13,57 @@ class User {
   String toString() {
     return "name: $name, age: $age";
   }
+}
+
+// - - - - - -  在模型类中序列化 JSON 数据 - - - - - -
+// https://docs.flutter.cn/data-and-backend/serialization/json
+
+class User2 {
+  final String name;
+  final String email;
+
+  User2(this.name, this.email);
+
+  // 用于从映射中构造一个新的 User 实例
+  User2.fromJson(Map<String, dynamic> json)
+      : name = json['name'] as String,
+        email = json['email'] as String;
+
+  // 将 User 实例转换为一个映射
+  Map<String, dynamic> toJson() => {'name': name, 'email': email};
+}
+
+// TODO 使用单元测试进行验证
+void testJson(String jsonString) {
+  // 解码获得一个 user 实例
+  final userMap = jsonDecode(jsonString) as Map<String, dynamic>;
+  final user = User2.fromJson(userMap);
+
+  // 对 user 实例进行编码
+  String json = jsonEncode(user);
+}
+
+// 以 json_serializable 的方式创建模型类
+
+@JsonSerializable()
+class User3 {
+  String name;
+  String email;
+
+  User3(this.name, this.email);
+
+  // dart run build_runner build --delete-conflicting-outputs
+  factory User3.fromJson(Map<String, dynamic> json) => _$User3FromJson(json);
+
+  Map<String, dynamic> toJson() => _$User3ToJson(this);
+}
+
+// 使用 json_serializable 模型
+void useJsonSerializable(String jsonString) {
+  // 解码获得一个 user 实例
+  final userMap = jsonDecode(jsonString) as Map<String, dynamic>;
+  final user = User3.fromJson(userMap);
+
+  // 对 user 实例进行编码
+  String json = jsonEncode(user);
 }
---
+++ flutter_study/lib/models/user.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User3 _$User3FromJson(Map<String, dynamic> json) => User3(
+      json['name'] as String,
+      json['email'] as String,
+    );
+
+Map<String, dynamic> _$User3ToJson(User3 instance) => <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+    };
