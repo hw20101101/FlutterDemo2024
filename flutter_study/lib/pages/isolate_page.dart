4b1234d989c64ac29170783232283db2137bd449
hwacdx <1296324895@qq.com>
2024-12-12 22:58:50 +0800

    完善在后台处理 JSON 数据解析

--- flutter_study/lib/pages/isolate_page.dart
+++ flutter_study/lib/pages/isolate_page.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Photo {
@@ -44,4 +45,64 @@ List<Photo> parsePhotos(String responseBody) {
   final parsed =
       (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
   return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+class IsolatePage extends StatefulWidget {
+  const IsolatePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _IsolatePageState();
+  }
+}
+
+class _IsolatePageState extends State<IsolatePage> {
+  late Future<List<Photo>> futurePhotos;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePhotos = fetchPhotos(http.Client());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: FutureBuilder<List<Photo>>(
+            future: futurePhotos,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('An error has occurred!'),
+                );
+              } else if (snapshot.hasData) {
+                return PhotoList(photos: snapshot.data!);
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
+  }
+}
+
+class PhotoList extends StatelessWidget {
+  const PhotoList({super.key, required this.photos});
+
+  final List<Photo> photos;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+      },
+    );
+  }
 }
