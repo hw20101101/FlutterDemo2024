f5234bbdc9c0b2ec6d036a5c6856b3eaca151b0c
hwacdx <1296324895@qq.com>
2025-04-26 22:20:28 +0800

    didUpdateWidget

--- flutter_lifecycle_250423/lib/page1.dart
+++ flutter_lifecycle_250423/lib/page1.dart
@@ -11,15 +11,19 @@ class _Page1State extends State<Page1> with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
+    //首次 调用顺序 1
     print("page1 initState......");
     WidgetsBinding.instance.addObserver(this); //注册监听器
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      //首次 调用顺序 5
       print("单次Frame绘制回调"); //只回调一次
     });
 
     WidgetsBinding.instance.addPersistentFrameCallback((_) {
+      //首次 调用顺序 4
       print("实时Frame绘制回调"); //每帧都回调
+      //update 调用顺序 3
     });
   }
 
@@ -36,13 +40,16 @@ class _Page1State extends State<Page1> with WidgetsBindingObserver {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    //首次 调用顺序 2
     print("page1 didChangeDependencies......");
   }
 
   //绘制界面
   @override
   Widget build(BuildContext context) {
+    //首次 调用顺序 3
     print("page1 build......");
+    //update 调用顺序 2
     return Scaffold(
       appBar: AppBar(
         title: Text('Lifecycle demo'),
@@ -58,5 +65,37 @@ class _Page1State extends State<Page1> with WidgetsBindingObserver {
         ),
       ),
     );
+  }
+
+  //状态改变的时候会调用该方法,比如父类调用了setState
+  @override
+  void didUpdateWidget(Page1 oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    //update 调用顺序 1
+    print("page1 didUpdateWidget......");
+  }
+
+  //当State对象从树中被移除时，会调用此回调
+  @override
+  void deactivate() {
+    super.deactivate();
+    print('page1 deactivate......');
+  }
+
+  //当State对象从树中被永久移除时调用；通常在此回调中释放资源
+  @override
+  void dispose() {
+    super.dispose();
+    print('page1 dispose......');
+    WidgetsBinding.instance.removeObserver(this); //移除监听器
+  }
+
+  //监听App生命周期回调
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    print("page1 state:$state");
+    if (state == AppLifecycleState.resumed) {
+      //do sth
+    }
   }
 }
